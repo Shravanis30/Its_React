5ada2f89ec93973ee1ad8c7d2bfd1570f7ff3d79
Shravanis30 <salunkeshravani561@gmail.com>
2024-07-06 21:07:56 +0530

    codes

--- 06_CurrencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06_CurrencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,22 +1,21 @@
 import { useEffect, useState } from "react";
-
-// hooks are purely js so js is used
+// These hooks are purely js so js is used
 
 // the unspoken rule of the hooks is to use the word "use" before the method name
 // hooks take optional argument but somtime it doesnt
-
+// api's by default type is string not json
 function useCurrencyInfo(currency) {
-        const [data, setData] = useState({});
-        useEffect(() => {
-            fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-                .then((res) => res.json())
-                .then((res) => setData(res[currency]))
-        }, [currency])
-
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdeliver.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
         console.log(data);
-        return data
-
-    }
-
+    }, [currency]) 
+    console.log(data)
+    return data
+}
 
 export default useCurrencyInfo;
+
+
